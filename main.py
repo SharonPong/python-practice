499a7b67ce9d53ecf9705b6a33d3de35f24f7f3a
SharonPong <sharon665523@gmail.com>
2022-08-11 16:19:31 +0800

    練習提交

---
+++ main.py
@@ -0,0 +1,25 @@
+test_by_sharon = 'Test By Sharon'
+
+
+print(test_by_sharon)
+
+
+"""
+1. 在github上開了一個倉庫？
+    -> 怎麼再開一個
+    已在github上開新倉庫 python-pratice_2
+    如何clone到pycharm上還在研究
+2. git 指令
+    -> git branch develop 查詢所有本地分支
+    -> git checkout develop 切換分支
+    -> git add <filename> 加入索引
+    -> git commit -m "project init" 提交更新版本
+    -> git push 將本地更新的branch堆送到遠端主機
+3 目前還推不上去，需要配置秘鑰
+"""
+
+
+"""
+
+
+"""
